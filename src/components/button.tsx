98b5dbd18578159a73d018218847ca5f43381a33
Gerion Herbst <gerion@gmx.com>
2025-08-03 17:01:57 +0200

    chore: fix code style

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,7 +4,7 @@ import { type ButtonHTMLAttributes, forwardRef } from 'react';
 import { cn } from '~/lib/cn';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-md text-base font-medium px-4 py-2 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'focus-visible:ring-ring inline-flex items-center justify-center rounded-md px-4 py-2 text-base font-medium transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
