00d216d7e63cfc5a5b195a7e6848db7a12984b54
Gerion Herbst <gerion@gmx.com>
2025-09-27 15:53:39 +0200

    chore(deps): bump deps
    - bump to tanstack start rc

--- src/router.tsx
+++ src/router.tsx
@@ -5,7 +5,7 @@ import { DefaultCatchBoundary } from './components/default-catch-boundary';
 import { NotFound } from './components/not-found';
 import { routeTree } from './routeTree.gen';
 
-export function createRouter() {
+export function getRouter() {
   const queryClient = new QueryClient();
 
   const router = routerWithQueryClient(
@@ -24,6 +24,6 @@ export function createRouter() {
 
 declare module '@tanstack/react-router' {
   interface Register {
-    router: ReturnType<typeof createRouter>;
+    router: ReturnType<typeof getRouter>;
   }
 }
